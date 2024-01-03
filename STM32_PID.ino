437fa8b3e5488c7757fc9babdd7f4c9a6232945f
Estebancott <estebancott@gmail.com>
2024-01-03 23:48:25 +0100

    Refactor STM32_PID.ino file

--- STM32_PID.ino
+++ STM32_PID.ino
@@ -1,8 +1,4 @@
-#include "FastInterruptEncoder.h"
 #include "PID.h"
-
-Encoder encDroit(PA0, PA1, SINGLE, 250); // - Example for STM32, check datasheet for possible Timers for Encoder mode. TIM_CHANNEL_1 and TIM_CHANNEL_2 only
-Encoder encGauche(PB4, PB5, SINGLE, 250);
 
 float dt = 10e-3; // 10ms
 
@@ -16,15 +12,23 @@ float Kp_D = 1, Ki_D = 1, Kd_D = 1; //coefficients PID vitesse moteur droit
 
 #ifdef useSimulation
   #include "SimFirstOrder.h"
+
   float process_sim_motor = 0; // variable qui va stocker la valeur de sortie de la simulation du moteur si on lui donne juste la consigne
   float Output_Sim_PID_vitesse_G= 0; // variable qui stocke la sortie du PID simulé pour le moteur Gauche
   float process_sim_motor_PID = 0; // variable qui va stocker la valeur de sortie de la simulation du moteur part rapport au PID simulé
+
   float K_Motor_G = 1;
   float tau_Motor_G = 1;
   SimFirstOrder simFirstOrder_G(dt, tau_Motor_G, K_Motor_G); // first order system for motor Gauche
   SimFirstOrder simFirstOrder_G2(dt, tau_Motor_G, K_Motor_G); // first order system simulation of motor Gauche
+
   PID Sim_PID_vitesse_G(&process_sim_motor_PID, &Output_Sim_PID_vitesse_G, &cmd_vitesse_G, dt, Kp_G, Ki_G, Kd_G, DIRECT);
 #else
+  #include "FastInterruptEncoder.h"
+
+  Encoder encDroit(PA0, PA1, SINGLE, 250); // - Example for STM32, check datasheet for possible Timers for Encoder mode. TIM_CHANNEL_1 and TIM_CHANNEL_2 only
+  Encoder encGauche(PB4, PB5, SINGLE, 250);
+
   float vitesse_G = 0; //vitesse réel moteur gauche
   float vitesse_D = 0; //vitesse réel moteur droite
 
@@ -35,24 +39,32 @@ float Kp_D = 1, Ki_D = 1, Kd_D = 1; //coefficients PID vitesse moteur droit
   PID PID_vitesse_D(&vitesse_D, &Output_PID_vitesse_D, &cmd_vitesse_D, dt, Kp_D, Ki_D, Kd_D, DIRECT);
 #endif
 
+#define DEBUG
+
 void setup()
 {
-  Serial.begin(115200);
-
-  if (encDroit.init() && encGauche.init())
-  {
-    Serial.println("Encoder Initialization OK");
-  }
-  else
-  {
-    Serial.println("Encoder Initialization Failed");
-    while (1)
-      ;
-  }
-
   #ifdef useSimulation
     Sim_PID_vitesse_G.SetMode(AUTOMATIC); //turn the PID on
   #else
+    #ifdef DEBUG
+      Serial.begin(115200);
+      if (encDroit.init() && encGauche.init())
+      {
+        Serial.println("Encoder Initialization OK");
+      }
+      else
+      {
+        Serial.println("Encoder Initialization Failed");
+        while (1)
+          ;
+      }
+    #else
+      if (!encDroit.init() && !encGauche.init())
+      {
+        while (1)
+          ;
+      }
+    #endif
     PID_vitesse_G.SetMode(AUTOMATIC); //turn the PID on
     PID_vitesse_D.SetMode(AUTOMATIC); //turn the PID on
   #endif
@@ -82,24 +94,27 @@ void Update_IT_callback(void)
   #else
     encDroit.loop();
     encGauche.loop();
-    Serial.print("Droit: ");
-    Serial.println(encDroit.getTicks());
-    Serial.print("Gauche: ");
-    Serial.println(encGauche.getTicks());
 
     vitesse_G = encGauche.getTicks()/dt;
     vitesse_D = encDroit.getTicks()/dt;
-    Serial.print("V_G : ");
-    Serial.println(vitesse_G,5);
-    Serial.print("V_D : ");
-    Serial.println(vitesse_D,5);
 
     PID_vitesse_G.Compute();
     PID_vitesse_D.Compute();
-    Serial.print("PID_V_G : ");
-    Serial.println(Output_PID_vitesse_G,5);
-    Serial.print("PID_V_D : ");
-    Serial.println(Output_PID_vitesse_D,5);
+    
+    #ifdef DEBUG
+      Serial.print("Droit: ");
+      Serial.println(encDroit.getTicks());
+      Serial.print("Gauche: ");
+      Serial.println(encGauche.getTicks());
+      Serial.print("V_G : ");
+      Serial.println(vitesse_G,5);
+      Serial.print("V_D : ");
+      Serial.println(vitesse_D,5);
+      Serial.print("PID_V_G : ");
+      Serial.println(Output_PID_vitesse_G,5);
+      Serial.print("PID_V_D : ");
+      Serial.println(Output_PID_vitesse_D,5);
+    #endif
   #endif
 }
 
