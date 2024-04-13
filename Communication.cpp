27fee9374865950286f9dd627c4fa11b24b5b0b1
Killian <picot.killian@outlook.fr>
2024-04-13 12:07:40 +0200

    Mise à jour de la fonction sendData() pour inclure les variables angle_ok et distance_ok.

--- Communication.cpp
+++ Communication.cpp
@@ -39,7 +39,6 @@ void asservCommandUSB(int argc, char **argv)
 		char side = *argv[1];
 		float speedGoal = atof(argv[2]);
 		int time = atoi(argv[3]);
-	
 
 		// float speedRight = speedGoal;
 		// float speedLeft = 0;
@@ -95,7 +94,8 @@ void usbSerialCallback(char *buffer, uint32_t size)
 		if (nb_arg > 0 && !strcmp(firstArg, "asserv"))
 		{
 			asservCommandUSB(nb_arg, argv);
-		} else
+		}
+		else
 		{
 			printUsage();
 		}
@@ -132,40 +132,44 @@ void serialEvent()
 /*************************************/
 void sendData()
 {
-    Serial.print("A"); // Valeur du codeur Gauche
-    Serial.println(last_encGauche);
-    Serial.print("B"); // Valeur du codeur Droit
-    Serial.println(last_encDroit);
-    Serial.print("C"); // Vitesse réel moteur Gauche
-    Serial.println(vitesse_G, 5);
-    Serial.print("D"); // Vitesse réel moteur Droit
-    Serial.println(vitesse_D, 5);
-    Serial.print("E"); // Sortie du PID vitesse moteur Gauche
-    Serial.println(Output_PID_vitesse_G, 5);
-    Serial.print("F"); // Sortie du PID vitesse moteur Droit
-    Serial.println(Output_PID_vitesse_D, 5);
-    Serial.print("G"); // Consigne de vitesse moteur Gauche
-    Serial.println(cmd_vitesse_G, 5);
-    Serial.print("H"); // Consigne de vitesse moteur Droit
-    Serial.println(cmd_vitesse_D, 5);
-    Serial.print("I"); // angle mesurer
-    Serial.println(angle,5);
-    Serial.print("J"); // angle PID
-    Serial.println(Output_PID_angle);
-    Serial.print("K");
-    Serial.println(cmd_angle, 5);
-    Serial.print("L"); // distance mesurer
-    Serial.println(distance, 5);
-    Serial.print("M"); // distance PID
-    Serial.println(Output_PID_distance, 5);
-    Serial.print("O"); // cmd distance
-    Serial.println(cmd_distance, 5);
-    Serial.print("X"); // position x
-    Serial.println(x);
-    Serial.print("Y"); // position y
-    Serial.println(y);
+	Serial.print("A"); // Valeur du codeur Gauche
+	Serial.println(last_encGauche);
+	Serial.print("B"); // Valeur du codeur Droit
+	Serial.println(last_encDroit);
+	Serial.print("C"); // Vitesse réel moteur Gauche
+	Serial.println(vitesse_G, 5);
+	Serial.print("D"); // Vitesse réel moteur Droit
+	Serial.println(vitesse_D, 5);
+	Serial.print("E"); // Sortie du PID vitesse moteur Gauche
+	Serial.println(Output_PID_vitesse_G, 5);
+	Serial.print("F"); // Sortie du PID vitesse moteur Droit
+	Serial.println(Output_PID_vitesse_D, 5);
+	Serial.print("G"); // Consigne de vitesse moteur Gauche
+	Serial.println(cmd_vitesse_G, 5);
+	Serial.print("H"); // Consigne de vitesse moteur Droit
+	Serial.println(cmd_vitesse_D, 5);
+	Serial.print("I"); // angle mesurer
+	Serial.println(angle, 5);
+	Serial.print("J"); // angle PID
+	Serial.println(Output_PID_angle);
+	Serial.print("K");
+	Serial.println(cmd_angle, 5);
+	Serial.print("L"); // distance mesurer
+	Serial.println(distance, 5);
+	Serial.print("M"); // distance PID
+	Serial.println(Output_PID_distance, 5);
+	Serial.print("O"); // cmd distance
+	Serial.println(cmd_distance, 5);
+	Serial.print("P"); // angle ok
+	Serial.println(angle_ok);
+	Serial.print("Q"); // distance ok
+	Serial.println(distance_ok);
+	Serial.print("X"); // position x
+	Serial.println(x);
+	Serial.print("Y"); // position y
+	Serial.println(y);
 
-    Update_IT = false;
+	Update_IT = false;
 }
 /*************************************/
 /*************************************/
