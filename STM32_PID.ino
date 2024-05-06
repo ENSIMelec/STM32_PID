8240f1ff76f0403e3054ce83f430610cd026e185
Killian <picot.killian@outlook.fr>
2024-05-06 18:03:55 +0200

    Mise à jour des fonctions goTo, rotate et moveOf dans Move.h et Move.cpp

--- STM32_PID.ino
+++ STM32_PID.ino
@@ -1,8 +1,8 @@
 
 #include "main.h"
-#include "PID.h"                   // bibliothèque PID
-#include "FastInterruptEncoder.h"  // bibliothèque pour les codeurs incrémentaux
-#include "SimFirstOrder.h"         // bibliothèque pour la simulation du moteur
+#include "PID.h"                  // bibliothèque PID
+#include "FastInterruptEncoder.h" // bibliothèque pour les codeurs incrémentaux
+#include "SimFirstOrder.h"        // bibliothèque pour la simulation du moteur
 #include "Communication.h"
 #include "interruption.h"
 #include "BoucleOuverte.h"
@@ -10,23 +10,23 @@
 #include "Move.h"
 
 /******Mode********/
-#define DEBUG  // mode debug
+#define DEBUG // mode debug
 /******************/
 
-bool debug = false;
+bool debug = true;
 
 unsigned long timeSetup;
 short mode = 4;
 /******ECHANTILLONAGE********/
-float dt = 10e-3;  // 10ms
+float dt = 10e-3; // 10ms
 volatile int Update_IT = 0;
 /****************************/
 
 /******CONSIGNES PID**********/
-float cmd_vitesse_G = 0;  // commande vitesse moteur gauche en mm/ms
-float cmd_vitesse_D = 0;  // commande vitesse moteur droite en mm/ms
-float cmd_angle = 0;      // commande angle
-float cmd_distance = 0;   // commande distance
+float cmd_vitesse_G = 0; // commande vitesse moteur gauche en mm/ms
+float cmd_vitesse_D = 0; // commande vitesse moteur droite en mm/ms
+float cmd_angle = 0;     // commande angle
+float cmd_distance = 0;  // commande distance
 float VMax = 500;
 float distance_final = 0;
 float angle_final = 0;
@@ -40,21 +40,21 @@ float distance_encoder_droit = 1000.0 / 4715.0;
 /**************************************/
 
 /********Coef Vitesse ******/
-float coefVitesseG = distance_encoder_gauche / dt;
+float coefVitesseG = distance_encoder_gauche / dt*1.05;
 float coefVitesseD = distance_encoder_droit / dt;
 /**************************/
 
 /********Coef Angle****/
-float correction_angle = 0.95;
+float correction_angle = 0.93;
 float empattementRoueCodeuse = 241;
 float coefAngle = dt / empattementRoueCodeuse * correction_angle;
 /**********************/
 
 /******COEFICIENTS PID************/
-float Kp_G = 100.0 / 475.0, Ki_G = 0.0, Kd_G = 0.00;         // coefficients PID vitesse moteur gauche
-float Kp_D = 100.0 / 500.0, Ki_D = 0.0, Kd_D = 0.00;         // coefficients PID vitesse moteur droit
-float Kp_angle = 3500, Ki_angle = 1600, Kd_angle = 0;        // coefficients PID angle
-float Kp_distance = 20, Ki_distance = 1.5, Kd_distance = 0;  // coefficients PID distance
+float Kp_G = 100.0 / 475.0, Ki_G = 0.0, Kd_G = 0.00;        // coefficients PID vitesse moteur gauche
+float Kp_D = 100.0 / 500.0, Ki_D = 0.0, Kd_D = 0.00;        // coefficients PID vitesse moteur droit
+float Kp_angle = 3500, Ki_angle = 1600, Kd_angle = 0;       // coefficients PID angle
+float Kp_distance = 20, Ki_distance = 1.5, Kd_distance = 0; // coefficients PID distance
 /*********************************/
 
 /*****ETAT DEPLACEMENT************/
@@ -68,8 +68,8 @@ int arret_lidar = 2;
 /******ENCODEUR************/
 int16_t last_encGauche = 0;
 int16_t last_encDroit = 0;
-Encoder encGauche(CodGB, CodGA, TIM3, &last_encGauche, HALFQUAD, 250);  // PWM2/1 pin A0 et PWM2/2 pin A1 Donc Timer 2 utilisé
-Encoder encDroit(CodDB, CodDA, TIM2, &last_encDroit, HALFQUAD, 250);    // PWM3/1 pin D5 et PWM3/2 pin D4 Donc Timer 3 utilisé
+Encoder encGauche(CodGB, CodGA, TIM3, &last_encGauche, HALFQUAD, 250); // PWM2/1 pin A0 et PWM2/2 pin A1 Donc Timer 2 utilisé
+Encoder encDroit(CodDB, CodDA, TIM2, &last_encDroit, HALFQUAD, 250);   // PWM3/1 pin D5 et PWM3/2 pin D4 Donc Timer 3 utilisé
 /***************************************/
 
 /*****Sauvegarde des positions*****/
@@ -78,17 +78,17 @@ float y = 0;
 /**********************************/
 
 /******Constante mesuré************/
-float vitesse_G = 0;  // vitesse gauche
-float vitesse_D = 0;  // vitesse droite
-float angle = 0;      // angle
-float distance = 0;   // distance
+float vitesse_G = 0; // vitesse gauche
+float vitesse_D = 0; // vitesse droite
+float angle = 0;     // angle
+float distance = 0;  // distance
 /**********************************/
 
 /******Corection PID************/
-float Output_PID_vitesse_G = 0;  // Valeur sortante du PID vitesse moteur gauche, une PMW donc
-float Output_PID_vitesse_D = 0;  // Valeur sortante du PID vitesse moteur droit, une PMW donc
-float Output_PID_angle = 0;      // Valeur sortante du PID angle
-float Output_PID_distance = 0;   // Valeur sortante du PID distance
+float Output_PID_vitesse_G = 0; // Valeur sortante du PID vitesse moteur gauche, une PMW donc
+float Output_PID_vitesse_D = 0; // Valeur sortante du PID vitesse moteur droit, une PMW donc
+float Output_PID_angle = 0;     // Valeur sortante du PID angle
+float Output_PID_distance = 0;  // Valeur sortante du PID distance
 /*******************************/
 
 /******Declaration des PID************/
@@ -105,9 +105,10 @@ PID PID_distance(&distance, &Output_PID_distance, &cmd_distance, dt, Kp_distance
 /*************************************/
 /*****SETUP***************************/
 /*************************************/
-void setup() {
+void setup()
+{
   /**********INITIALISATION COMMUNICATION SÉRIE*/
-  Serial.begin(115200);  // Par défaut utilisation de USART1
+  Serial.begin(115200); // Par défaut utilisation de USART1
   /*********************************************/
   Serial.println("Serial OK");
 
@@ -116,9 +117,12 @@ void setup() {
 #ifdef DEBUG
   /****************************/
   /********MODE DEBUG***********/
-  if (encDroit.init() && encGauche.init()) {
+  if (encDroit.init() && encGauche.init())
+  {
     Serial.println("-Encoder Initialization OK");
-  } else {
+  }
+  else
+  {
     Serial.println("-Encoder Initialization Failed");
     while (1)
       ;
@@ -130,24 +134,25 @@ void setup() {
 #else
   /****************************/
   /********MODE NORMAL*********/
-  if (!encDroit.init() && !encGauche.init()) {
+  if (!encDroit.init() && !encGauche.init())
+  {
     while (1)
-      ;  // encoder initialization failed
+      ; // encoder initialization failed
   }
   /****************************/
   /****************************/
 #endif
 
   /******Initialisation des PINs****/
-  pinMode(DIR1, OUTPUT);  // PA_3 = pin D0
-  pinMode(DIR2, OUTPUT);  // PA_2 = pin D1
-  pinMode(PWM1, OUTPUT);  // PWM4/1 pin D10 donc le Timer4
-  pinMode(PWM2, OUTPUT);  // PWM1/1 pin D7 donc le Timer1
+  pinMode(DIR1, OUTPUT); // PA_3 = pin D0
+  pinMode(DIR2, OUTPUT); // PA_2 = pin D1
+  pinMode(PWM1, OUTPUT); // PWM4/1 pin D10 donc le Timer4
+  pinMode(PWM2, OUTPUT); // PWM1/1 pin D7 donc le Timer1
   /*********************************/
 
   // delay(5000);
-  pinMode(LED_BUILTIN, OUTPUT);     // Configure la broche de la LED comme sortie
-  digitalWrite(LED_BUILTIN, HIGH);  // Allume LED Confirmation d'initialisation
+  pinMode(LED_BUILTIN, OUTPUT);    // Configure la broche de la LED comme sortie
+  digitalWrite(LED_BUILTIN, HIGH); // Allume LED Confirmation d'initialisation
   /******Activation des PID************/
   encDroit.resetTicks();
   encGauche.resetTicks();
@@ -168,7 +173,7 @@ void setup() {
 
   change_PID_mode(4);
 
-  NVIC_SetPriority(TIM5_IRQn, 1);  // Priorité pour l'interruption du timer
+  NVIC_SetPriority(TIM5_IRQn, 1); // Priorité pour l'interruption du timer
 
   timeSetup = millis();
 }
@@ -179,11 +184,14 @@ void setup() {
 /*************************************/
 /*****LOOP**************************/
 /*************************************/
-void loop() {
-  if (Update_IT >= 10 && debug) {
+void loop()
+{
+  if (Update_IT >= 10 && debug)
+  {
     sendData();
   }
-  if (send_new_command_available && arret_lidar >= 2) {
+  if (send_new_command_available && arret_lidar >= 2)
+  {
     Serial.println("Z");
     send_new_command_available = false;
   }
